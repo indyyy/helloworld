dd9b8b49febe93385919baff1868940bf7006000
Inder <inderyyz@gmail.com>
2018-09-06 18:04:58 +1000

    color change

--- docs/app.js
+++ docs/app.js
@@ -1,2 +1,8 @@
-heading = document.querySelector("h1");
-heading.textContent = "Hello World";
+heading = document.querySelector("#test");
+heading.textContent = "What's up Aman";
+
+for (i = 1; i < 100; i++) {
+  for (j = 1; j < 100; j++) {
+    $("#test").append("<span id=test>What's up Aman </span>");
+  }
+}
